3803023bd5c5999ef50a551c0e4718ddd5407ebc
Dale Gordon <dbgordon09@gmail.com>
2019-09-12 14:22:56 +0200

    Dimens + Colors + Footer

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Route } from 'react-router-dom';
 import Navbar from '../layouts/Navbar/Navbar';
 import Container from '../layouts/Container/Container';
+import Footer from '../layouts/Footer/Footer';
 import Router from '../utils/Router';
 
 const App = () => (
-  <div>
+  <div className="App">
     <Router>
       <Navbar />
       <Container>
         <Route path="/todo" component={Navbar} />
       </Container>
+      <Footer />
     </Router>
   </div>
 );
---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Footer.module.scss';
+import links from './Links';
+
+export default () => (
+  <footer className={styles.navbar}>
+    <nav className="flex h-full">
+      <Link to="/" className={`${styles.navbar__home} h-full pr-4`}>
+        <img
+          className={`${styles.navbar__logo} h-full float-left`}
+          src="/images/logo.svg"
+          alt="navbar_logo"
+        />
+      </Link>
+      {links.map((link) => (
+        <div className="h-full table" key={link.url}>
+          <Link
+            to={link.url}
+            className={`${styles.navbar__links} h-full table-cell align-middle`}
+          >
+            {link.title}
+          </Link>
+        </div>
+      ))}
+    </nav>
+  </footer>
+);
