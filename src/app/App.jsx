647bac47bb681227c62f458f9def910715dff3b4
Dale Gordon <dbgordon09@gmail.com>
2019-09-16 22:36:28 +0200

    Added Redux + Fixed Linting Quirks
    Ignored AirBnb line breaks.
    Added Axios, Redux and React Redux.
    Changed Tailwind building to tailwind build script.
    Wrapped App in Redux store provider.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.scss';
 import { Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from '../layouts/Navbar/Navbar';
 import Container from '../layouts/Container/Container';
 import Footer from '../layouts/Footer/Footer';
 import Router from '../utils/Router';
+import store from '../store';
 
 const App = () => (
-  <div className="App">
-    <Router>
-      <Navbar />
-      <Container>
-        <Route path="/todo" component={Navbar} />
-      </Container>
-      <Footer />
-    </Router>
-  </div>
+  <Provider store={store}>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Container>
+          <Route path="/todo" component={Navbar} />
+        </Container>
+        <Footer />
+      </Router>
+    </div>
+  </Provider>
 );
 
 export default App;
