ee1c4c3bcfd20b1a2083ed61555219deea3902cd
Dale Gordon <dbgordon09@gmail.com>
2019-09-18 21:28:31 +0200

    Added JSDocs

--- src/layouts/Navbar/Links.js
+++ src/layouts/Navbar/Links.js
@@ -1,4 +1,10 @@
 class Link {
+  /**
+     * Generate a new Link instance
+     * @class
+     * @param  {string} title The title to be displayed
+     * @param  {string} url The destination of the link
+     */
   constructor(title, url) {
     this.title = title;
     this.url = url;
