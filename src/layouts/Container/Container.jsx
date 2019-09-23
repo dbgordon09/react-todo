0fd9e49940c59094ffd8c01c739a85ed360224c6
Dale Gordon <dbgordon09@gmail.com>
2019-09-23 15:36:53 +0200

    Functional Component Documentation v2

--- src/layouts/Container/Container.jsx
+++ src/layouts/Container/Container.jsx
@@ -3,15 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './Container.module.scss';
 
 /**
- * Container for page based content to be rendered into
+ * Container for page based content to be rendered into.
  *
- * NOTE: SHOULD BE PASSED A CHILD TO RENDER
- *
- * @type {React.FunctionComponentElement<{
- *  children : React.oneOfType({
- *    React.node[],
- *    React.node})
- * }>}
+ * NOTE: SHOULD BE PASSED A CHILDREN TO RENDER.
+ * @type {React.FunctionComponentElement<React.ReactNode|React.ReactNodeArray>}
+ * @param {React.ReactNode|ReactNodeArray} children - Content to be rendered within the container.
  */
 const Container = ({ children }) => (
   <div className={`${styles.container}`}>{children}</div>
